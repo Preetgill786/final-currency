d828cb229a1b56d7513e2b50f8b5ed1d5963976f
Preetgill786 <57011583+Preetgill786@users.noreply.github.com>
2019-11-15 14:33:50 -0500

    Add files via upload

--- currencyconverter/AppDelegate.swift
+++ currencyconverter/AppDelegate.swift
@@ -12,8 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 var window: UIWindow?
-
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       Thread.sleep(forTimeInterval: 3)
+        
         // Override point for customization after application launch.
         return true
     }
